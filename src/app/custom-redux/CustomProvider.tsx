11aa1e4b998cf91e5df17fdc2d9f0bc916443bbb
oleh.hrinishyn <oleh.hrinishyn@eleks.com>
2021-06-30 09:23:40 +0300

    ready to implement

--- src/app/custom-redux/CustomProvider.tsx
+++ src/app/custom-redux/CustomProvider.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import {CustomStore} from './custom-store';
-import {customReducer} from './example-reducer';
-
-export class CustomProvider extends React.Component<{ store: CustomStore }, any> {
-    constructor(props) {
-        super(props);
-        props.store.injectReducer(customReducer)
-    }
-
-    render() {
-        return this.props.children;
-    }
-}
+export const CustomProvider = {}
